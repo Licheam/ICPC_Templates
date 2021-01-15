e4bf2d74db87e41b1dd45990cc455e6ff416d3c6
Michaelwmx <wmx_wmx@foxmail.com>
2021-01-15 11:24:17 +0800

    update dijkstra

--- 图论/最短路Djikstra.cpp
+++ 图论/最短路Djikstra.cpp
@@ -1,76 +1,80 @@
-#include <cstdio>
-#include <cstring>
-#include <climits>
-#include <set>
-
+#include <bits/stdc++.h>
 #define LL long long
-#define MAXN 100005
-#define MAXM 500005
-
 using namespace std;
+const int inf=0x3f3f3f3f;
+const LL INF=0x3f3f3f3f3f3f3f3f;
+const int MOD=998244353;
+const int MAXN=2000005;
 
-struct edge{
-	int v,next,to;
-}e[MAXM];
+struct edge {
+	int to,next,w;
+}e[MAXN<<1];
 
-int n,m,p,tot=0,dist[MAXN],head[MAXN];
+int tot,head[MAXN];
 
-void add(int x, int y, int z){
+void add(int x,int y,int z) {
 	tot++;
-	e[tot].v=z;
 	e[tot].to=y;
+	e[tot].w=z;
 	e[tot].next=head[x];
 	head[x]=tot;
 }
 
-struct node{
-	int v,u;
-	bool operator<(const node a)const{
-		if(v!=a.v)
-			return v<a.v;
-		else
-			return u<a.u;
+struct node {
+	int u;
+	LL dis;
+	bool operator<(const node& y) const{
+		return dis>y.dis;
 	}
-	node(int x,int y):v(x),u(y){}
 };
 
-set<node> s;//v,u
-set<node>::iterator ite;
-int u,q;
-LL tem;
+LL dist[MAXN];
+bool flag[MAXN];
+priority_queue<node> Q;
 
-void djikstra(int x){
-	for(int i=1;i<=n;i++){
-		dist[i]=INT_MAX;
-	}
-	dist[x]=0;
-	s.insert(node(0,x));
-	while(!s.empty()){
-		ite=s.begin();
-		u=ite->u;
-		s.erase(ite);
-		for(int q=head[u];q;q=e[q].next){
-			int v=e[q].to,w=e[q].v;
-			if(dist[u]+w<dist[v]){
-				ite=s.find(node(dist[v],v));
-				if(ite!=s.end()) s.erase(ite);
+void dij(int n,int s) {
+	memset(flag+1,0,n*sizeof(flag[0]));
+	memset(dist+1,0x3f,n*sizeof(dist[0]));
+	dist[s]=0;
+	Q.push({s,0LL});
+	while(!Q.empty()) {
+		while(!Q.empty() && flag[Q.top().u]) Q.pop();
+		if(Q.empty()) break;
+		int u=Q.top().u;
+		Q.pop();
+		flag[u]=1;
+		for(int p=head[u];p;p=e[p].next) {
+			int v=e[p].to,w=e[p].w;
+			if(flag[v]) continue;
+			if(dist[u]+w<dist[v]) {
 				dist[v]=dist[u]+w;
-				s.insert(node(dist[v],v));
+				Q.push({v,dist[v]});
 			}
 		}
 	}
 }
 
-int main(){
-	scanf("%d %d %d", &n, &m ,&p);
-	memset(head+1,0,sizeof(head[0])*n);
-	for(int i=1;i<=m;i++){
+void solve() {
+	int n,m,s;
+	scanf("%d %d %d", &n, &m, &s);
+	tot=0;
+	memset(head+1,0,n*sizeof(head[0]));
+	for(int i=1;i<=m;i++) {
 		int f,g,w;
 		scanf("%d %d %d", &f, &g, &w);
 		add(f,g,w);
 	}
-	djikstra(p);
+	dij(n,s);
 	for(int i=1;i<=n;i++)
-		printf("%d ", dist[i]);
+		printf("%lld%c", dist[i], " \n"[i==n]);
+}
+
+int main() {
+	int T=1,cas=1;(void)(cas);
+	// scanf("%d", &T);
+	while(T--) {
+		// printf("Case #%d: ", cas++);
+		solve();
+	}
 	return 0;
 }
