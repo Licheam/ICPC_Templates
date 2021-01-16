4c3649c08e071ea16dc55a5fede915648d84d5fd
Michaelwmx <wmx_wmx@foxmail.com>
2021-01-17 00:58:48 +0800

    update KMP

--- 字符串/字符串匹配KMP.cpp
+++ 字符串/字符串匹配KMP.cpp
@@ -1,38 +1,40 @@
-#include <cstdio>
-#include <cstring>
+#include <bits/stdc++.h>
+using namespace std;
 #define MAXN 1000005
 
-int n,m,cnt,next[MAXN];
-char s1[MAXN],s2[MAXN];
+int kmp(char *s1, char *s2, int *nxt){
+    int n=strlen(s1+1),m=strlen(s2+1);
 
-void kmp(){
-	next[0]=-1;
-	for(int i=1,k=-1;i<=m;i++){
-		//k初始值为-1. next数组可以查询boarder的boarder.
-		while(~k && s2[k+1]!=s2[i])
-			k=next[k];
-		next[i]=++k;
-	}
+    nxt[0]=-1;
+    for(int i=1,k=-1;i<=m;i++){
+        //k初始值为-1. nxt数组可以查询boarder的boarder.
+        while(~k && s2[k+1]!=s2[i])
+            k=nxt[k];
+        nxt[i]=++k;
+    }
 
-	cnt=0;
-	for(int i=1,k=0;i<=n;i++){
-		//匹配串前缀与模式串后缀比，上一位前缀的boarder的boarder也能匹配。
-		while(~k && s2[k+1]!=s1[i])
-			k=next[k];
-		if(m==++k){
-			cnt++;
-			printf("%d\n", i-m+1);
-		}
-	}
+    int cnt=0;
+    for(int i=1,k=0;i<=n;i++){
+        //匹配串前缀与模式串后缀比，上一位前缀的boarder的boarder也能匹配。
+        while(~k && s2[k+1]!=s1[i])
+            k=nxt[k];
+        if(m==++k){
+            cnt++;
+            printf("%d\n", i-m+1);
+        }
+    }
+    return cnt;
 }
 
+char s1[MAXN],s2[MAXN];
+int nxt[MAXN];
+
 int main(){
-	scanf("%s %s", s1+1, s2+1);
-	n=strlen(s1+1);
-	m=strlen(s2+1);
-	kmp();
-	for(int i=1;i<=m;i++){
-		printf("%d ", next[i]);
-	}
-	return 0;
+    scanf("%s %s", s1+1, s2+1);
+    kmp(s1,s2,nxt);
+    int len=strlen(s2+1);
+    for(int i=1;i<=len;i++){
+        printf("%d ", nxt[i]);
+    }
+    return 0;
 }
