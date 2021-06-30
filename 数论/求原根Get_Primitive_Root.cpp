8c3c1b663f64bbf1bd83785a9a09f82269cc1a75
Leachim <32847549+Michaelwmx@users.noreply.github.com>
2021-06-30 23:06:33 +0800

    update primitive root

--- 数论/求原根Get_Primitive_Root.cpp
+++ 数论/求原根Get_Primitive_Root.cpp
@@ -1,43 +1,36 @@
-#include <cstdio>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int p;
-vector<int> v;
-
-int binpow(int x,int y,int mod){
-	int r=1%mod;
-	while(y){
-		if(y&1) r=(1LL*r*x)%mod;
-		x=(1LL*x*x)%mod;
-		y>>=1;
-	}
-	return r;
+int binpow(int x,int y,int m){
+    int r=1%m;
+    while(y){
+        if(y&1) r=1LL*r*x%m;
+        x=1LL*x*x%m;
+        y>>=1;
+    }
+    return r;
 }
 
-int main(){
-	scanf("%d", &p);
-	int pp=p-1;
-	for(int i=2;i*i<=pp;i++){
-		if(pp%i==0){
-			v.push_back(i);
-			while(pp%i==0) pp/=i;
-		}
-	}
-	if(pp>1) v.push_back(pp);
-	for(int g=2;;g++){
-		bool isg=true;
-		for(int d:v){
-			if(binpow(g,(p-1)/d,p)==1){
-				isg=false;
-				break;
-			}
-		}
-		if(isg){
-			printf("%d\n", g);
-			break;
-		}
-	}
-	return 0;
+int Primitive_Root(int p){//只能求素数
+    static vector<int> v;
+    v.clear();
+    int x=p-1;
+    for(int i=2;i*i<=x;i++) {
+        if(x%i==0){
+            v.push_back(i);
+            while(x%i==0) x/=i;
+        }
+    }
+    if(x>1) v.push_back(x);
+    for(int g=2;g<p;g++) {//原根一般很小
+        bool flag=true;
+        for(auto x:v) {
+            if(binpow(g,(p-1)/x,p)==1){
+                flag=false;
+                break;
+            }
+        }
+        if(flag) return g;
+    }
+    return 0;
 }
