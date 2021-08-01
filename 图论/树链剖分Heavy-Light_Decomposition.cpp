184593dde4969dd25a4dbddf6f4dc881f4029bb0
Leachim <32847549+Michaelwmx@users.noreply.github.com>
2021-08-01 12:41:24 +0800

    modified HLD

--- 图论/树链剖分Heavy-Light_Decomposition.cpp
+++ 图论/树链剖分Heavy-Light_Decomposition.cpp
@@ -1,14 +1,16 @@
 /*
-	重链剖分
-	单点修改
-	路径最值
-	路径和
+	重链剖分luogu3384
+	路径修改
+	路径和查询
+	子树修改
+	子树和查询
 */
 #include <bits/stdc++.h>
 #define LL long long
 using namespace std;
 const int inf=0x3f3f3f3f;
 const int MAXN=2000005;
+int P;
 
 struct edge {
 	int to,next;
@@ -57,13 +59,21 @@ void dfs2(int x,int f) {
 }
 
 struct SGT{
-	LL sum[MAXN<<2];
-	int mx[MAXN<<2];
+	int sum[MAXN<<2],tag[MAXN<<2];
 
-	void pushup(int x) {sum[x]=sum[x<<1]+sum[x<<1|1];mx[x]=max(mx[x<<1],mx[x<<1|1]);}
+	void pushup(int x) {sum[x]=(1LL*sum[x<<1]+sum[x<<1|1])%P;}
+	void pushdown(int x,int l,int r) {
+		int m=(l+r)>>1;
+		sum[x<<1]=(sum[x<<1]+1LL*tag[x]*(m-l+1)%P)%P;
+		tag[x<<1]=(1LL*tag[x<<1]+tag[x])%P;
+		sum[x<<1|1]=(sum[x<<1|1]+1LL*tag[x]*(r-m)%P)%P;
+		tag[x<<1|1]=(1LL*tag[x<<1|1]+tag[x])%P;
+		tag[x]=0;
+	}
 
 	void build(int x,int l,int r) {
-		if(l==r) sum[x]=mx[x]=w[rnk[l]];
+		tag[x]=0;
+		if(l==r) sum[x]=w[rnk[l]]%P;
 		else {
 			int m=(l+r)>>1;
 			build(x<<1,l,m);
@@ -72,96 +82,92 @@ struct SGT{
 		}
 	}
 
-	void modify(int x,int l,int r,int qx,int v) {
-		if(l==qx && r==qx) {
-			sum[x]=v;
-			mx[x]=v;
+	void modify(int x,int l,int r,int ql,int qr,int delta) {
+		if(ql<=l && r<=qr) {
+			sum[x]=(sum[x]+1LL*delta*(r-l+1)%P)%P;
+			tag[x]=(1LL*tag[x]+delta)%P;
 			return;
 		}
+		if(tag[x]) pushdown(x,l,r);
 		int m=(l+r)>>1;
-		if(qx<=m) modify(x<<1,l,m,qx,v);
-		if(m<qx) modify(x<<1|1,m+1,r,qx,v);
+		if(ql<=m) modify(x<<1,l,m,ql,qr,delta);
+		if(m<qr) modify(x<<1|1,m+1,r,ql,qr,delta);
 		pushup(x);
 	}
 
-	LL qsum(int x,int l,int r,int ql,int qr) {
+	int query(int x,int l,int r,int ql,int qr){
 		if(ql<=l && r<=qr) return sum[x];
 
+		if(tag[x]) pushdown(x,l,r);
 		int m=(l+r)>>1;
 		LL res=0;
-		if(ql<=m) res+=qsum(x<<1,l,m,ql,qr);
-		if(m<qr) res+=qsum(x<<1|1,m+1,r,ql,qr);
-		return res;
-	}
-
-	int qmax(int x,int l,int r,int ql,int qr) {
-		if(ql<=l && r<=qr) return mx[x];
-
-		int m=(l+r)>>1;
-		int res=-inf;
-		if(ql<=m) res=max(res,qmax(x<<1,l,m,ql,qr));
-		if(m<qr) res=max(res,qmax(x<<1|1,m+1,r,ql,qr));
+		if(ql<=m) res=(1LL*res+query(x<<1,l,m,ql,qr))%P;
+		if(m<qr) res=(1LL*res+query(x<<1|1,m+1,r,ql,qr))%P;
 		return res;
 	}
 }sgt;
 
 void solve() {
-	int n;
-	scanf("%d", &n);
+	int n,m,s;
+	scanf("%d %d %d %d", &n, &m, &s, &P);
 	tot=0;
 	memset(head+1,0,n*sizeof(head[0]));
+	for(int i=1;i<=n;i++)
+		scanf("%d", &w[i]);
 	for(int i=1;i<n;i++) {
 		int f,g;
 		scanf("%d %d", &f, &g);
 		add(f,g);
 		add(g,f);
 	}
-	for(int i=1;i<=n;i++)
-		scanf("%d", &w[i]);
 	dep[0]=0;
-	dfs1(1,0);
+	dfs1(s,0);
 	cnt=0;
-	dfs2(1,1);
+	dfs2(s,s);
 
 	sgt.build(1,1,n);
 
-	int q;
-	scanf("%d", &q);
-	for(int i=1;i<=q;i++) {
-		char opt[10];
-		int x,y;
-		scanf("%s %d %d", opt, &x, &y);
-		if(opt[0]=='C') { //w[x]=y;
-			w[x]=y;
-			sgt.modify(1,1,n,dfn[x],y);
-		} else if(opt[1]=='M') { //max(x,y)
-			int mx=-inf;
+	for(int i=1;i<=m;i++) {
+		int opt;
+		scanf("%d", &opt);
+		if(opt==1) { //w(x,y)+z;
+			int x,y,z;
+			scanf("%d %d %d", &x, &y, &z);
 			while(top[x]!=top[y]) {
 				if(dep[top[x]]>dep[top[y]]) {
-					mx=max(mx,sgt.qmax(1,1,n,dfn[top[x]],dfn[x]));
+					sgt.modify(1,1,n,dfn[top[x]],dfn[x],z);
 					x=fa[top[x]];
 				} else {
-					mx=max(mx,sgt.qmax(1,1,n,dfn[top[y]],dfn[y]));
+					sgt.modify(1,1,n,dfn[top[y]],dfn[y],z);
 					y=fa[top[y]];
 				}
 			}
-			if(dep[x]>dep[y]) mx=max(mx,sgt.qmax(1,1,n,dfn[y],dfn[x]));
-			else mx=max(mx,sgt.qmax(1,1,n,dfn[x],dfn[y]));
-			printf("%d\n", mx);
-		} else {// sum(x,y);
-			LL sum=0;
+			if(dep[x]>dep[y]) sgt.modify(1,1,n,dfn[y],dfn[x],z);
+			else sgt.modify(1,1,n,dfn[x],dfn[y],z);
+		} else if(opt==2) { //sum(x,y)
+			int x,y;
+			scanf("%d %d", &x, &y);
+			int sum=0;
 			while(top[x]!=top[y]) {
 				if(dep[top[x]]>dep[top[y]]) {
-					sum+=sgt.qsum(1,1,n,dfn[top[x]],dfn[x]);
+					sum=(1LL*sum+sgt.query(1,1,n,dfn[top[x]],dfn[x]))%P;
 					x=fa[top[x]];
 				} else {
-					sum+=sgt.qsum(1,1,n,dfn[top[y]],dfn[y]);
+					sum=(1LL*sum+sgt.query(1,1,n,dfn[top[y]],dfn[y]))%P;
 					y=fa[top[y]];
 				}
 			}
-			if(dep[x]>dep[y]) sum+=sgt.qsum(1,1,n,dfn[y],dfn[x]);
-			else sum+=sgt.qsum(1,1,n,dfn[x],dfn[y]);
-			printf("%lld\n", sum);
+			if(dep[x]>dep[y]) sum=(1LL*sum+sgt.query(1,1,n,dfn[y],dfn[x]))%P;
+			else sum=(1LL*sum+sgt.query(1,1,n,dfn[x],dfn[y]))%P;
+			printf("%d\n", sum);
+		} else if(opt==3){// w(x.tree)+z
+			int x,z;
+			scanf("%d %d", &x, &z);
+			sgt.modify(1,1,n,dfn[x],dfn[x]+siz[x]-1,z);
+		} else { // sum(x.tree)
+			int x;
+			scanf("%d", &x);
+			printf("%d\n", sgt.query(1,1,n,dfn[x],dfn[x]+siz[x]-1));
 		}
 	}
 }
